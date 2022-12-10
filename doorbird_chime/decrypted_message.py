394183fc48c09a05c0935ff0a182f147f1b8e87b
Michael Bass <michael.a.bass@hotmail.co.uk>
2022-12-10 17:53:05 +0000

    Add message handling.

---
+++ doorbird_chime/decrypted_message.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+from datetime import *
+
+class DecryptedMessage:
+    def __init__(self, cleartext):
+        self.cleartext = cleartext
+        # The ID of the device sending the alert.
+        self.id = cleartext[:6].decode('utf-8')
+        # The type of event sent this will either be 'motion' or the product number of the device sending the alert.
+        self.event = cleartext[6:14].decode('utf-8')
+        # The UTC time as a timestamp.
+        self.timestamp = datetime.utcfromtimestamp(int.from_bytes(cleartext[14:], 'big'))
+
+    def __eq__(self, other: DecryptedMessage) -> bool:
+        return self.cleartext == other.cleartext
+
+    def __str__(self) -> str:
+        return f'ID {id}\nEVENT {self.event}\nTIMESTAMP {self.timestamp}'
---
+++ doorbird_chime/encrypted_message.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+from nacl import pwhash, secret, bindings
+from decrypted_message import DecryptedMessage
+
+class EncryptedMessage:
+    def __init__(self, message_bytes = b'\xde\xad\xbe\x01'):
+        self.message_bytes = message_bytes
+        # Check for the doorbird signature.
+        if message_bytes[:4] != b'\xde\xad\xbe\x01':
+            raise ValueError('Not a doorbird event.')
+        # This is the doorbird signature in its individual pieces.
+        self.ident = message_bytes[:3]
+        self.version = message_bytes[4]
+        try:
+            # Try to get the decryption variables and encrypted cyphertext.
+            self.opslimit = int.from_bytes(message_bytes[4:8], 'big')
+            self.memlimit = int.from_bytes(message_bytes[8:12], 'big')
+            self.salt = message_bytes[12:28]
+            self.nonce = message_bytes[28:36]
+            self.ciphertext = message_bytes[36:]
+        except:
+            pass
+
+    def __eq__(self, other: EncryptedMessage) -> bool:
+        # If a message has identical bytes then it is the same message.
+        return self.message_bytes == other.message_bytes
+
+    def __str__(self) -> str:
+        return f'IDENT {self.ident}\nVERSION {self.version}\nOPSLIMIT {self.opslimit}\nMEMLIMIT {self.memlimit}\nSALT {self.salt}\nNONCE {self.nonce}\nCIPHERTEXT {self.ciphertext}'
+
+    def decrypt(self, passwd: str) -> DecryptedMessage:
+        """
+        Decrypt the message using the provided password.
+        """
+        # Only the last 5 characters of the password are used!?
+        passwd = passwd[:5].encode('utf-8')
+        # Use the password and encryption variables to make a key.
+        key = pwhash.argon2i.kdf(secret.SecretBox.KEY_SIZE, passwd, self.salt, opslimit = self.opslimit, memlimit = self.memlimit)
+        # Use the key and nonce to decrypt the ciphertext.
+        cleartext = bindings.crypto_aead_chacha20poly1305_decrypt(self.ciphertext, None, self.nonce, key)
+        # Build a DecryptedMessage from the cleartext.
+        return DecryptedMessage(cleartext)
