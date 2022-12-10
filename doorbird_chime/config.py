fc536cd25e1c6e3d1cc2982b1d896781f7796760
Michael Bass <michael.a.bass@hotmail.co.uk>
2022-12-10 18:08:36 +0000

    Add config reader.

---
+++ doorbird_chime/config.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import yaml
+
+class Config:
+    def __init__(self, conf_path: Path):
+        # Open the config file and read it safely as yaml.
+        with conf_path.open() as conf_file:
+            config = yaml.safe_load(conf_file)
+        # Pull config settings from the yaml.
+        self.password = config['password']
+        self.user = config['user']
+        self.chime_sound = config['sound_file']
+        self.sleep_start = config['sleep_start']
+        self.sleep_end = config['sleep_end']
