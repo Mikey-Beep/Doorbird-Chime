ae2b29d342e93f05560124c7a280fdbeddc714a3
Michael Bass <michael.a.bass@hotmail.co.uk>
2022-12-10 18:57:17 +0000

    Add watcher.

---
+++ doorbird_chime/doorbird_watcher.py
@@ -0,0 +1,48 @@
+from socket import *
+from config import Config
+from pathlib import Path
+from encrypted_message import EncryptedMessage
+from chime import Chime
+
+class DoorbirdWatcher:
+    def __init__(self, port: int):
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.bind(('', port))
+    
+    def watch(self):
+        # Build a blank message for comparison.
+        last_message = EncryptedMessage()
+        print('Monitoring for doorbell events.')
+        while 1:
+            mess = self.socket.recvfrom(1024)[0]
+            # See if this is a message from the doorbell.
+            try:
+                mess = EncryptedMessage(mess)
+            except ValueError:
+                print('Not a doorbird message.')
+                continue
+            # See if this is a duplicate of the last message.
+            if mess == last_message:
+                print('Duplicate event.')
+                continue
+            # Store a copy of this message for future comparison.
+            last_mess = mess
+            print(mess)
+            # Load config from file, this lets us dynamically update the config.
+            conf = Config(Path(__file__).parent / 'conf' / 'conf.yml')
+            # Decode the message.
+            try:
+                decoded = mess.decrypt(conf.password)
+            except:
+                continue
+            print(decoded)
+            # Check that this event is for the correct user and that it is not a motion detection event.
+            if conf.user[:6] == decoded.id and not decoded.event.startswith('motion'):
+                print('Matched user name and this is a button press!')
+                chime = Chime(conf)
+                chime.make_noise()
+
+
+if __name__ == '__main__':
+    watcher = DoorbirdWatcher(6524)
+    watcher.watch()
