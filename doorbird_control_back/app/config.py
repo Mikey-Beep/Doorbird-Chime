cf853b6945b51b60a9f68527ae51db9418a8e08a
Michael Bass <michael.a.bass@hotmail.co.uk>
2022-12-11 22:06:23 +0000

    Add basic config management.

---
+++ doorbird_control_back/app/config.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import yaml
+
+class ConfigManager:
+    def __init__(self):
+        self.conf_path = Path(__file__).parent.parent / 'conf' / 'conf.yml'
+        self.conf_path.parent.mkdir(parents = True, exist_ok = True)
+        try:
+            with self.conf_path.open() as conf_file:
+                self.config = yaml.safe_load(conf_file)
+        except FileNotFoundError:
+            self.config = self.build_bare_config()
+        
+    def build_bare_config(self):
+        config = {
+            'password': '',
+            'user': '',
+            'sound_file': '',
+            'sleep_start': '',
+            'sleep_end': ''
+        }
+        self.save_config()
+        return config
+
+    def save_config(self):
+        with self.conf_path.open('w') as conf_file:
+            conf_file.write(yaml.safe_dump(self.config))
--- doorbird_control_back/app/serv.py
+++ doorbird_control_back/app/serv.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 from test_broadcast import TestBroadcaster
+from config import ConfigManager
 
 app = Flask(__name__)
+config_manager = ConfigManager()
 
-@app.route('/test_broadcast')
+@app.route('/test_broadcast', methods = ['GET'])
 def test_broadcast():
     broadcaster = TestBroadcaster()
-    broadcaster.broadcast()
+    broadcaster.broadcast(config_manager.conf_path['test_packet'])
+
+@app.route('/config', methods = ['POST'])
+def write_config():
+    config_manager.config['user'] = request.json['user']
+    config_manager.config['password'] = request.json['password']
+    config_manager.config['sound_file'] = request.json['sound_file']
+    config_manager.config['sleep_start'] = request.json['sleep_start']
+    config_manager.config['sleep_end'] = request.json['sleep_end']
+    try:
+        config_manager.config['test_packet'] = request.json['test_packet']
+    except:
+        pass
+    config_manager.save_config()
 
 app.run(host='0.0.0.0')
