c39ffe426a1dc4fa70cb898fc3ad66008bf4c4a5
Michael Bass <michael.a.bass@hotmail.co.uk>
2022-12-26 17:10:54 +0000

    Populate config.

--- doorbird_control_front/app/serv.py
+++ doorbird_control_front/app/serv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, Response
+import requests, json
 
 app = Flask(__name__)
 
@@ -14,5 +14,17 @@ def send_test_packet():
     response = requests.request("GET", url, data='')
     print(response.text)
     return render_template('index.html')
+
+@app.route('/get_config')
+def get_config():
+    url = 'http://back/config'
+    resp = requests.request("GET", url, data='')
+    return Response(status = 200, response = json.dumps(resp.json()))
+
+@app.route('/get_sounds')
+def get_sounds():
+    url = 'http://back/sound_files'
+    resp = requests.request("GET", url, data='')
+    return Response(status = 200, response = json.dumps(resp.json()))
 
 app.run(host='0.0.0.0', port = 80)
