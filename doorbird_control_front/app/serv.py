f4e9bf72e9670db4f66a4a25895db317d98fdfb1
Michael Bass <michael.a.bass@hotmail.co.uk>
2022-12-26 13:38:50 +0000

    Implement sending test packet.

--- doorbird_control_front/app/serv.py
+++ doorbird_control_front/app/serv.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/send_test')
+def send_test_packet():
+    url = 'http://back/test_broadcast'
+    response = requests.request("GET", url, data='')
+    print(response.text)
     return render_template('index.html')
 
 app.run(host='0.0.0.0', port = 80)
