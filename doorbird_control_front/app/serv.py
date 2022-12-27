632401a307eb00a6685e23a7928e8e522df1e773
Michael Bass <Mikey-Beep@users.noreply.github.com>
2022-12-27 11:05:55 +0000

    Initial UI Creation (#3)
    * feat: Add front to docker compose.
    
    * feat: Send test packet.
    
    * feat: Populate config.
    
    * feat: Set config.
    
    * feat: Upload sound files.

---
+++ doorbird_control_front/app/serv.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template, Response, request
+import requests, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send_test')
+def send_test_packet():
+    url = 'http://back/test_broadcast'
+    response = requests.request("GET", url, data='')
+    print(response.text)
+    return render_template('index.html')
+
+@app.route('/get_config')
+def get_config():
+    url = 'http://back/config'
+    resp = requests.request("GET", url, data='')
+    return Response(status = 200, response = json.dumps(resp.json()))
+
+@app.route('/send_config', methods = ['POST'])
+def send_config():
+    url = 'http://back/config'
+    requests.request("POST", url, json=request.json)
+    return Response(status = 200)
+
+@app.route('/send_sound', methods = ['POST'])
+def send_sound():
+    requests.request(
+        method='POST',
+        url='http://back/sound_file',
+        files={k: (v.filename, v.stream, v.content_type, v.headers) for k, v in request.files.items()}
+    )
+    return Response(status = 200)
+
+@app.route('/get_sounds')
+def get_sounds():
+    url = 'http://back/sound_files'
+    resp = requests.request("GET", url, data='')
+    return Response(status = 200, response = json.dumps(resp.json()))
+
+app.run(host='0.0.0.0', port = 80)
