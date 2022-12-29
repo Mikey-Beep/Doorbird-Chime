fa0ef59823a78b613614edbe67cbdaf584f9aad5
Michael Bass <michael.a.bass@hotmail.co.uk>
2022-12-29 14:03:23 +0000

    refactor: General refactor.

--- doorbird_chime/app/doorbird_watcher.py
+++ doorbird_chime/app/doorbird_watcher.py
@@ -10,40 +10,38 @@ class DoorbirdWatcher:
         self.socket = socket(AF_INET, SOCK_DGRAM)
         self.socket.bind(('', port))
     
-    def watch(self):
-        # Build a blank message for comparison.
-        last_message = EncryptedMessage()
+    def watch(self, last_message: EncryptedMessage = EncryptedMessage()):
         print('Monitoring for doorbell events.')
         while 1:
-            mess = self.socket.recvfrom(1024)[0]
+            inbound_message = self.socket.recvfrom(1024)[0]
             # See if this is a message from the doorbell.
             try:
-                mess = EncryptedMessage(mess)
+                encrypted_message = EncryptedMessage(inbound_message)
             except ValueError:
                 continue
             # Load config from file, this lets us dynamically update the config.
             conf = Config(Path(__file__).parent.parent / 'conf' / 'conf.yml')
             # See if this is a duplicate of the last message.
-            not_test_packet = mess != EncryptedMessage(base64.b64decode(conf.test_packet.encode('ascii')))
-            if mess == last_message and not_test_packet:
+            is_test_packet = encrypted_message != EncryptedMessage(base64.b64decode(conf.test_packet.encode('ascii')))
+            if encrypted_message == last_message and not is_test_packet:
                 print('Duplicate event.')
                 continue
-            print(mess)
+            print(encrypted_message)
             # Store a copy of this message for future comparison.
-            if not_test_packet:
-                last_message = mess
+            if not is_test_packet:
+                last_message = encrypted_message
                 log_path = Path(__file__).parent.parent / 'log' / 'message_log.txt'
                 log_path.parent.mkdir(parents=True, exist_ok=True)
                 with log_path.open('w'):
-                    log_path.write_text(f'{mess.message_bytes}\n')
+                    log_path.write_text(f'{encrypted_message.message_bytes}\n')
             # Decode the message.
             try:
-                decoded = mess.decrypt(conf.password)
+                decoded_message = encrypted_message.decrypt(conf.password)
             except:
                 continue
-            print(decoded)
+            print(decoded_message)
             # Check that this event is for the correct user and that it is not a motion detection event.
-            if conf.user[:6] == decoded.id and not decoded.event.startswith('motion'):
+            if conf.user[:6] == decoded_message.id and not decoded_message.event.startswith('motion'):
                 print('Matched user name and this is a button press!')
                 chime = Chime(conf)
                 chime.make_noise()
