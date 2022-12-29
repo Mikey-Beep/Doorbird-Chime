1e41ee71982e9a1425d576fdab5b754130b46429
Michael Bass <michael.a.bass@hotmail.co.uk>
2022-12-29 18:48:07 +0000

    feat: Rolling log to keep the latest 100 entries.

--- doorbird_chime/app/logger.py
+++ doorbird_chime/app/logger.py
@@ -6,10 +6,12 @@ class DoorbirdLogger:
         self.log_path = Path(__file__).parent.parent / 'log' / 'log.txt'
 
     def log(self, message: DecryptedMessage):
-        with self.log_path.open('a+') as log_file:
-            log_file.write('\u16bc'.join([
+        with self.log_path.open() as log_file:
+            logs = [line.strip() for line in log_file]
+        with self.log_path.open('w+') as log_file:
+            logs.append('\u16bc'.join([
                 message.id,
                 message.event,
                 str(message.timestamp)
             ]))
-            log_file.write('\n')
+            log_file.write('\n'.join(logs[-100:]))
