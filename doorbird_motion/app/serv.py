c2426f243b1df62b219922d94b783907efd14c90
Michael Bass <michael.a.bass@hotmail.co.uk>
2022-12-30 16:29:32 +0000

    feat: Store motion event images.

--- doorbird_motion/app/serv.py
+++ doorbird_motion/app/serv.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 from flask import Flask, request, Response
 from config import Config
+from datetime import datetime
 import requests
 
 config_path = Path(__file__).parent.parent / 'conf' / 'conf.yml'
+image_directory_path = Path(__file__).parent.parent / 'images'
 
 app = Flask(__name__)
 
@@ -14,6 +16,9 @@ def record_motion():
     session.auth = (config.user, config.password)
     response = session.get(f'http://{config.doorbell_address}/bha-api/image.cgi')
     print(response.content)
+    image_path = image_directory_path / f'{datetime.utcnow()}.jpg'
+    with image_path.open('wb') as image_file:
+        image_file.write(response.content)
     return Response(status = 200)
 
 app.run(host = '0.0.0.0', port = 80)
