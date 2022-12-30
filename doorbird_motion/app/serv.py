468fa3887382b0d7d8c43404c6d2c31a8162f814
Michael Bass <michael.a.bass@hotmail.co.uk>
2022-12-30 09:31:11 +0000

    feat: Request image from doorbell.

--- doorbird_motion/app/serv.py
+++ doorbird_motion/app/serv.py
@@ -1,11 +1,19 @@
+from pathlib import Path
 from flask import Flask, request, Response
 from config import Config
+import requests
+
+config_path = Path(__file__).parent.parent / 'conf' / 'conf.yml'
 
 app = Flask(__name__)
 
 @app.route('/motion')
 def record_motion():
-    config = Config()
+    config = Config(config_path)
+    session = requests.Session()
+    session.auth = (config.user, config.password)
+    response = session.get(f'{config.doorbell_address}/bha-api/image.cgi')
+    print(response.content)
     return Response(status = 200)
 
 app.run(host = '0.0.0.0', port = 80)
