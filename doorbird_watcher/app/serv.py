5b3225bf3611ea13464db6b428d4b36a1a7be711
Michael Bass <michael.a.bass@hotmail.co.uk>
2023-01-22 20:27:43 +0000

    feat: Make motion capture async.

--- doorbird_watcher/app/serv.py
+++ doorbird_watcher/app/serv.py
@@ -2,8 +2,7 @@ from pathlib import Path
 from flask import Flask, request, Response
 from config import Config
 from watcher import Watcher
-from datetime import datetime
-import time
+import threading
 
 app = Flask(__name__)
 config_path = Path(__file__).parent.parent / 'conf' / 'conf.yml'
@@ -12,21 +11,8 @@ watcher = Watcher(config)
 
 @app.route('/motion', methods = ['GET'])
 def motion():
-    # Grab the most recent 3 images from the watcher.
-    images = watcher.images.copy()
-    # Grab two more images.
-    time.sleep(5)
-    images.append(watcher.get_current_image())
-    time.sleep(5)
-    images.append(watcher.get_current_image())
-    # Build a directory to store the files for this motion event.
-    motion_directory = Path(__file__).parent.parent / 'images' / 'motion' / str(datetime.utcnow())
-    motion_directory.mkdir(parents = True, exist_ok = True)
-    # Save the images into this directory.
-    for i in range(len(images)):
-        image_path = motion_directory / f'{i}.jpg'
-        with image_path.open('wb') as image_file:
-            image_file.write(images[i])
+    watcher_thread = threading.Thread(target = watcher.save_event_set, name = 'Watcher', args = ('motion',))
+    watcher_thread.start()
     return Response(status = 200)
 
 app.run(host = '0.0.0.0', port = 80)
--- doorbird_watcher/app/watcher.py
+++ doorbird_watcher/app/watcher.py
@@ -1,4 +1,6 @@
 from config import Config
+from pathlib import Path
+from datetime import datetime
 import requests, threading, time
 
 class Watcher:
@@ -21,3 +23,20 @@ class Watcher:
             time.sleep(5)
             self.images.append(self.get_current_image())
             self.images = self.images[-3:]
+    
+    def save_event_set(self, event_name: str):
+        # Grab the most recent 3 images from the watcher.
+        images = self.images.copy()
+        # Grab two more images.
+        time.sleep(5)
+        images.append(self.get_current_image())
+        time.sleep(5)
+        images.append(self.get_current_image())
+        # Build a directory to store the files for this event.
+        image_dir = Path(__file__).parent.parent / 'images' / event_name / str(datetime.utcnow())
+        image_dir.mkdir(parents = True, exist_ok = True)
+        # Save the images into this directory.
+        for i in range(len(images)):
+            image_path = image_dir / f'{i}.jpg'
+            with image_path.open('wb') as image_file:
+                image_file.write(images[i])
