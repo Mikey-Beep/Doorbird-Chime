32a811120f5940083182f16a105b50e6c251ed38
Michael Bass <michael.a.bass@hotmail.co.uk>
2023-01-24 08:13:30 +0000

    fix: Authentication on image retrieval.

--- doorbird_watcher/app/watcher.py
+++ doorbird_watcher/app/watcher.py
@@ -2,6 +2,7 @@ from config import Config
 from pathlib import Path
 from datetime import datetime
 import requests, threading, time
+from requests.auth import HTTPDigestAuth
 
 class Watcher:
     def __init__(self, config: Config):
@@ -11,10 +12,7 @@ class Watcher:
         self.watcher_thread.start()
 
     def get_current_image(self):
-            session = requests.Session()
-            session.auth = (self.config.user, self.config.password)
-            session.verify = False
-            response = session.get(f'https://{self.config.doorbell_ip}/bha-api/image.cgi')
+            response = requests.get(f'https://{self.config.doorbell_ip}/bha-api/image.cgi', auth = HTTPDigestAuth(self.config.user, self.config.password), verify = False)
             print('Image retrieved from doorbell.')
             return response.content
 
