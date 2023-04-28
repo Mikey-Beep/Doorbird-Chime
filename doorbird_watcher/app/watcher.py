bb0cac5fa900924f67367d4a42d8e73996583409
Michael Bass <Mikey-Beep@users.noreply.github.com>
2023-04-28 12:41:37 +0100

    Add ability to turn on light. (#25)
    * refactor: Tidy up watcher code.
    
    * feat: Add control backend endpoint to trigger the light.
    
    * feat: Send ir trigger request from control front to back.
    
    * feat: Add UI for triggering light and reloading image.
    
    * feat: Automatically refresh current image.

--- doorbird_watcher/app/watcher.py
+++ doorbird_watcher/app/watcher.py
@@ -8,16 +8,16 @@ urllib3.disable_warnings()
 class Watcher:
     def __init__(self, doorbell_ip: str, user: str, password: str, event_retention_count: int, image_spacing: int = 5):
         self.doorbell_ip = doorbell_ip
-        self.user = user
-        self.password = password
+        self.auth = HTTPDigestAuth(user, password)
         self.event_retention_count = event_retention_count
         self.image_spacing = image_spacing
+        self.image_dir = Path(__file__).parent.parent / 'images'
         self.images = [self.get_current_image()]
         self.watcher_thread = threading.Thread(target = self.watch, name = 'Watcher')
         self.watcher_thread.start()
 
     def get_current_image(self) -> bytes:
-            response = requests.get(f'https://{self.doorbell_ip}/bha-api/image.cgi', auth = HTTPDigestAuth(self.user, self.password), verify = False)
+            response = requests.get(f'https://{self.doorbell_ip}/bha-api/image.cgi', auth = self.auth, verify = False)
             return response.content
 
     def watch(self) -> None:
@@ -27,7 +27,7 @@ class Watcher:
             self.images = self.images[-3:]
     
     def drop_old_images(self, event_name: str) -> None:
-        event_dir = Path(__file__).parent.parent / 'images' / event_name
+        event_dir = self.image_dir / event_name
         events = sorted([event for event in event_dir.iterdir()], key = lambda x: x.name)
         if len(events) > self.event_retention_count:
             for i in range(len(events) - self.event_retention_count):
@@ -43,7 +43,7 @@ class Watcher:
         time.sleep(self.image_spacing)
         images.append(self.get_current_image())
         # Build a directory to store the files for this event.
-        image_dir = Path(__file__).parent.parent / 'images' / event_name / str(datetime.utcnow())
+        image_dir = self.image_dir / event_name / str(datetime.utcnow())
         image_dir.mkdir(parents = True, exist_ok = True)
         # Save the images into this directory.
         for i in range(len(images)):
